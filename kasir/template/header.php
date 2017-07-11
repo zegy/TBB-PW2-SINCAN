c4750dbeb20282489b33ebcc573682898afbf168
Agus Hariansyah <agusheriansyah89@gmail.com>
2017-07-11 16:51:53 +0700

    perubahan bentuk template html KASIR ke bentuk php

--- kasir/template/header.php
+++ kasir/template/header.php
@@ -1,16 +1,59 @@
- <!-- Page Content -->
-    <div class="container">
+<html lang="en">
+<head>
+<title>Website Salon Kecantikan</title>
+      
+  <head>
+    <meta charset="utf-8">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet"></link>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  </head>
 
-        <!-- Page Heading/Breadcrumbs -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Blog Home One
-                    <small>Subheading</small>
-                </h1>
-                <ol class="breadcrumb">
-                    <li><a href="index.html">Home</a>
-                    </li>
-                    <li class="active">Blog Home One</li>
-                </ol>
-            </div>
+  <body>
+    <div class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><span>Beranda</span></a>
         </div>
+        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="login_admin.html">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-2">
+            <img src="Images\kimcil.png"
+            class="img-responsive">
+          </div>
+          <div class="col-md-8">
+            <h2 align="center">Login Success ! Anda login sebagai Kasir</h2>
+          </div>
+          <div class="col-md-2">
+            <img src="Images\kimcil.png"
+            class="img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <hr>
+          </div>
+        </div>
+      </div>
+    </div>
