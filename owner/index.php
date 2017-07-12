9aae64e9e87d33342e718c53b1e763f6384cad47
dhiga007 <dhiga007@gmail.com>
2017-07-12 16:55:54 +0700

    lanjutan mengubah halaman html ke php

---
+++ owner/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+include_once 'template/navigasi.php';
+include_once 'template/header.php';
+include_once 'template/sidebar.php';
+include_once 'template/main.php';
+include_once 'template/footer.php';
+
+ ?>
